4f8cc87d9cd98f305ff02ea47dd0ce30cec8cbcf
Nguyenhoanglan1531998 <75198422+Nguyenhoanglan1531998@users.noreply.github.com>
2022-10-19 22:37:24 +0700

    update

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'Anhnm123!',
+  'database': 'postgres',
+  'host': 'project3-db.crvthafzjavv.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'anhnm-123',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
